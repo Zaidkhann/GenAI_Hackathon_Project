58cee6bd3ddb4758f732e847a825fe7dba171658
Zaid khan <mkzaid46@gmail.com>
2026-04-06 15:01:46 +0530

    Setup api_key Proble

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -1,6 +1,8 @@
+import dotenv from "dotenv";
+dotenv.config();
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const genAI = new GoogleGenerativeAI('AIzaSyB_dDPbX_46fNw72XSuEK04Bnr7S8pI9VM');
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 const model = genAI.getGenerativeModel({
   model: "gemini-2.5-flash"
--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
-import express from "express";
 import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
 import cors from "cors";
 
 import connectDB from "./config/db.js";
 import aiRoutes from "./routes/Ai.routes.js";
 
-dotenv.config();
+
 
 const app = express();
 
